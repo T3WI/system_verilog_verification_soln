b191ddf5ddd845b54a543dfc62539a7693d04220
Karlo Godfrey Escalona Gregorio <kgregorio9@yahoo.com>
2025-04-18 23:43:32 -0700

    Update 2_2.sv

--- chapter_2/2_2.sv
+++ chapter_2/2_2.sv
@@ -6,7 +6,13 @@
         my_logicmem[1] = 4'd1;
         ...
     Lastly, my_logic is assigned a 4-bit hex number. Remember, 1 hex bit is 4 binary bits.
-    UNFINISHED
+    2a. my_logicmem[4] would produce an out of bounds error, so the assignment would result in error. 
+    2b. my_logicmem[4] would produce an out of bounds error, so the assignment would result in error.
+    2c. The port sizes differ for the assignment, so it'll result in some error.
+    2d. 
+    2e. 
+    2f.
+    2g. 
 */
 `timescale 1ns/1ps
 module test;
@@ -18,19 +24,24 @@ module test;
         my_mem = '{default:8'hA5};
         my_logicmem = '{0, 1, 2, 3};
         my_logic = 4'hF;
-        #1000;
-        my_mem[2] = my_logicmem[4];
         #100;
-        my_logic = my_logicmem[4];
+
+
+        my_mem[2] = my_logicmem[3];
+        $display("2a: my_mem[2] = %x", my_mem[2]);
+        #100;
+        my_logic = my_logicmem[3];
+        $display("2b: my_logic = %x", my_logic);
         #100;
         my_logicmem[3] = my_mem[3];
+        $display("2c: my_logicmem[3] = %x", my_logicmem[3]);
         #100;
-        my_mem[3] = my_logic;
-        #100;
-        my_logic = my_logicmem[1];
-        #100;
-        my_logic = my_mem[1];
-        #100;
-        my_logic = my_logicmem[my_logicmem[41]];
+        // my_mem[3] = my_logic;
+        // #100;
+        // my_logic = my_logicmem[1];
+        // #100;
+        // my_logic = my_mem[1];
+        // #100;
+        // my_logic = my_logicmem[my_logicmem[41]];
     end
 endmodule 
